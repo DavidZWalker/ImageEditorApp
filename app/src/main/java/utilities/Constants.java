1e48cb978b44d81d276448213d6ea5dd1a27bd5f
fs126 <fs126@hdm-stuttgart.de>
2019-12-16 13:10:31 +0100

    unsplash api keys

--- app/src/main/java/utilities/Constants.java
+++ app/src/main/java/utilities/Constants.java
@@ -4,4 +4,7 @@ public final class Constants {
     public static final int CAMERA_PAGE = 1;
     public static final int LIBRARY_PAGE = 2;
     public static final int SEARCH_PAGE = 3;
+
+    public static final String UNSPLASH_ACCESS_KEY ="7823a3e208b0868365e231a019b604cf2df2e3c70e1e084d8758b6696164208a";
+    public static final String UNSPLASH_SECRET_KEY="44d14fd815a6fc7d501f87e95a98ae5b8ce5276f6f36cd4b4568f23703e1bb45";
 }
