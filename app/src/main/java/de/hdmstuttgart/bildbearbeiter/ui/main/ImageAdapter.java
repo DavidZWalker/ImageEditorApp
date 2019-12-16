cae1517aa2ab16ac9eb3d274f7aae667f03b8ec9
David Zack Walker <davidzzwalker@gmail.com>
2019-12-16 17:00:26 +0100

    - implemented basic fullscreen image view

--- app/src/main/java/de/hdmstuttgart/bildbearbeiter/ui/main/ImageAdapter.java
+++ app/src/main/java/de/hdmstuttgart/bildbearbeiter/ui/main/ImageAdapter.java
@@ -1,16 +1,23 @@
 package de.hdmstuttgart.bildbearbeiter.ui.main;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import de.hdmstuttgart.bildbearbeiter.R;
+import utilities.ImageFileHandler;
 
 public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
     private List<Bitmap> bitmapList;
@@ -27,9 +34,25 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
 
     @NonNull
     @Override
-    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ImageViewHolder onCreateViewHolder(@NonNull final ViewGroup parent, int viewType) {
         //create new view
         ImageView imageView = (ImageView) LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item,parent,false);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(parent.getContext(), FullscreenImageActivity.class);
+                ImageView imageView = (ImageView) v;
+                Bitmap bmp = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+                ImageFileHandler ifh = new ImageFileHandler(parent.getContext(), "tmp");
+                try {
+                    ifh.saveImage(bmp, "tmpImage");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                intent.putExtra("imageURI", "tmpImage");
+                parent.getContext().startActivity(intent);
+            }
+        });
         return new ImageViewHolder(imageView);
     }
 
