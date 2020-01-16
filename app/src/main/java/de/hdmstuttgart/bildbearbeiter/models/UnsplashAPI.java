2038b3d28ce3cff170feec57a4c177b6159606eb
fs126 <fs126@hdm-stuttgart.de>
2020-01-16 13:43:32 +0100

    added JavaDoc UnsplashAPI.java

--- app/src/main/java/de/hdmstuttgart/bildbearbeiter/models/UnsplashAPI.java
+++ app/src/main/java/de/hdmstuttgart/bildbearbeiter/models/UnsplashAPI.java
@@ -5,7 +5,19 @@ import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Query;
 
+/**
+ * The interface Unsplash api it contains all necessary attributes to make a valid request to unsplash API.
+ */
 public interface UnsplashAPI {
+    /**
+     * Gets search results.
+     *
+     * @param query    the query
+     * @param page     the page result number
+     * @param per_page images per page
+     * @param auth     the authentication token
+     * @return the search results
+     */
     @GET("search/photos")
     Call<SearchResponseResult> getSearchResults(@Query("query") String query, @Query("page") String page, @Query("per_page") String per_page, @Header("Authorization") String auth);
 }
