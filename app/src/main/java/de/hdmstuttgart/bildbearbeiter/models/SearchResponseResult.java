c967488a19dac35a421770da4787e181f73daae8
fs126 <fs126@hdm-stuttgart.de>
2020-01-16 13:42:07 +0100

    added JavaDoc SearchResponseResult.java

--- app/src/main/java/de/hdmstuttgart/bildbearbeiter/models/SearchResponseResult.java
+++ app/src/main/java/de/hdmstuttgart/bildbearbeiter/models/SearchResponseResult.java
@@ -5,6 +5,9 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
+/**
+ * The type Search response result is the "layout" of the response received by the unsplash API.
+ */
 public class SearchResponseResult {
     @SerializedName("total")
     @Expose
@@ -16,18 +19,36 @@ public class SearchResponseResult {
     @Expose
     private List<Photo> results = null;
 
+    /**
+     * Gets all photos.
+     *
+     * @return the photos
+     */
     public List<Photo> getPhotos() {
         return results;
     }
 
+    /**
+     * Gets size of the photo list
+     *
+     * @return the total
+     */
     public Integer getTotal() {
         return total;
     }
 
+    /**
+     * Sets total.
+     *
+     * @param total the total
+     */
     public void setTotal(Integer total) {
         this.total = total;
     }
 
+    /**
+     * The type Photo.
+     */
     public class Photo {
 
         @SerializedName("id")
@@ -43,15 +64,28 @@ public class SearchResponseResult {
         @Expose
         private Urls urls;
 
+        /**
+         * Gets urls.
+         *
+         * @return the urls
+         */
         public Urls getUrls() {
             return urls;
         }
 
+        /**
+         * Sets urls.
+         *
+         * @param urls the urls
+         */
         public void setUrls(Urls urls) {
             this.urls = urls;
         }
     }
 
+    /**
+     * The type Urls.
+     */
     public class Urls {
 
         @SerializedName("raw")
@@ -70,34 +104,74 @@ public class SearchResponseResult {
         @Expose
         private String thumb;
 
+        /**
+         * Gets full resolution.
+         *
+         * @return the full
+         */
         public String getFull() {
             return full;
         }
 
+        /**
+         * Sets full resolution.
+         *
+         * @param full the full
+         */
         public void setFull(String full) {
             this.full = full;
         }
 
+        /**
+         * Gets regular resolution.
+         *
+         * @return the regular
+         */
         public String getRegular() {
             return regular;
         }
 
+        /**
+         * Sets regular resolution.
+         *
+         * @param regular the regular
+         */
         public void setRegular(String regular) {
             this.regular = regular;
         }
 
+        /**
+         * Gets small resolution.
+         *
+         * @return the small
+         */
         public String getSmall() {
             return small;
         }
 
+        /**
+         * Sets small resolution.
+         *
+         * @param small the small
+         */
         public void setSmall(String small) {
             this.small = small;
         }
 
+        /**
+         * Gets thumb resolution.
+         *
+         * @return the thumb
+         */
         public String getThumb() {
             return thumb;
         }
 
+        /**
+         * Sets thumb resolution.
+         *
+         * @param thumb the thumb
+         */
         public void setThumb(String thumb) {
             this.thumb = thumb;
         }
