bce450c521020bc457b54a35ad04f2ab1dcb282e
fs126 <fs126@hdm-stuttgart.de>
2020-01-16 13:02:45 +0100

    added JavaDoc for BottomSheet.java and Camera.java

--- app/src/main/java/de/hdmstuttgart/bildbearbeiter/models/Camera.java
+++ app/src/main/java/de/hdmstuttgart/bildbearbeiter/models/Camera.java
@@ -8,33 +8,69 @@ import java.io.IOException;
 
 import de.hdmstuttgart.bildbearbeiter.utilities.ImageFileHandler;
 
+/**
+ * The type Camera.
+ */
 public class Camera {
     private Uri imageUri;
     private ImageFileHandler capturedImageFileHandler;
     private ImageFileHandler saveImageFileHandler;
     private String capturedImageTmpFileName = "capturedImage";
 
+    /**
+     * Instantiates a new Camera.
+     *
+     * @param appFilesDir the app files directory
+     */
     public Camera(File appFilesDir) {
         capturedImageFileHandler = new ImageFileHandler(appFilesDir, ImageFileHandler.IMAGE_DIR_TMP);
         saveImageFileHandler = new ImageFileHandler(appFilesDir, ImageFileHandler.IMAGE_DIR_LIB);
     }
 
+    /**
+     * Saves image to the internal image library.
+     *
+     * @param imageToSave the image to save
+     * @param fileName    the file name
+     * @throws IOException if the image couldn't be saved.
+     */
     public void saveImageToLibrary(Bitmap imageToSave, String fileName) throws IOException {
         saveImageFileHandler.saveImage(imageToSave, fileName);
     }
 
+    /**
+     * Create captured image file file.
+     *
+     * @return the file
+     */
     public File createCapturedImageFile() {
         return capturedImageFileHandler.createFileWithName(capturedImageTmpFileName);
     }
 
+    /**
+     * Gets captured bitmap from {@link ImageFileHandler}
+     *
+     * @return the captured bitmap
+     * @throws IOException if the picture couldn't be retrived.
+     */
     public Bitmap getCapturedBitmap() throws IOException {
         return capturedImageFileHandler.getImage(capturedImageTmpFileName);
     }
 
+    /**
+     * Sets image uri.
+     *
+     * @param imageUri the image uri
+     */
     public void setImageUri(Uri imageUri) {
         this.imageUri = imageUri;
     }
 
+    /**
+     * Gets image uri.
+     *
+     * @return the image uri
+     */
     public Uri getImageUri() {
         return this.imageUri;
     }
