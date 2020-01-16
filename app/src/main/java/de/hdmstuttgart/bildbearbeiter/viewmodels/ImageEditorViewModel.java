190e7ecd4c926dd65efdafa301ebc39186297c56
fs126 <fs126@hdm-stuttgart.de>
2020-01-16 14:33:45 +0100

    added JavaDoc ImageEditorViewModel.java

--- app/src/main/java/de/hdmstuttgart/bildbearbeiter/viewmodels/ImageEditorViewModel.java
+++ app/src/main/java/de/hdmstuttgart/bildbearbeiter/viewmodels/ImageEditorViewModel.java
@@ -14,19 +14,37 @@ import java.util.List;
 import de.hdmstuttgart.bildbearbeiter.filters.IBitmapFilter;
 import de.hdmstuttgart.bildbearbeiter.models.ImageEditor;
 
+/**
+ * The model for the image editor.
+ */
 public class ImageEditorViewModel extends ViewModel {
 
     private ImageEditor model;
 
+    /**
+     * Instantiates a new Image editor view model.
+     *
+     * @param appFilesDir the app files directory
+     */
     public ImageEditorViewModel(File appFilesDir) {
         super();
         model = new ImageEditor(appFilesDir);
     }
 
+    /**
+     * List of all currently available Filters
+     *
+     * @return the available filters
+     */
     public List<IBitmapFilter> getAvailableFilters() {
         return model.getAvailableFilters();
     }
 
+    /**
+     * Loads image from source.
+     *
+     * @return the source image
+     */
     public Bitmap getSourceImage() {
         try {
             return model.getSourceImage();
@@ -36,6 +54,12 @@ public class ImageEditorViewModel extends ViewModel {
         }
     }
 
+    /**
+     * Saves image to the in-app library.
+     *
+     * @param bitmapToSave the bitmap to save
+     * @return the boolean
+     */
     public boolean saveImageToLibrary(Bitmap bitmapToSave) {
         try {
             model.saveImageToLibrary(bitmapToSave);
@@ -46,6 +70,11 @@ public class ImageEditorViewModel extends ViewModel {
         }
     }
 
+    /**
+     * Creates a temporary BlackBitmap which is displayed during the process of applying a filter to a Bitmap.
+     *
+     * @return temporary BlackBitmap
+     */
     public Bitmap createTempBlackBitmap() {
         try {
             int width = model.getSourceImage().getWidth();
