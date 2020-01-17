9dcb6d2cc1ee52aec0cc612f9bcc23b4ba45c240
fs126 <fs126@hdm-stuttgart.de>
2020-01-17 12:21:34 +0100

    Added JavaDoc for IBitmapFilter.java

--- app/src/main/java/de/hdmstuttgart/bildbearbeiter/filters/IBitmapFilter.java
+++ app/src/main/java/de/hdmstuttgart/bildbearbeiter/filters/IBitmapFilter.java
@@ -2,8 +2,21 @@ package de.hdmstuttgart.bildbearbeiter.filters;
 
 import android.graphics.Bitmap;
 
+/**
+ * The interface Bitmap filter. It reqires every Filter to have an applyFilter() and getName().
+ */
 public interface IBitmapFilter {
+    /**
+     * This method is supposed to apply a Filter onto the entire Bitmap.
+     *
+     * @return the bitmap after applying it with a filter
+     */
     Bitmap applyFilter();
 
+    /**
+     * Gets the name of the filter
+     *
+     * @return the name
+     */
     String getName();
 }
